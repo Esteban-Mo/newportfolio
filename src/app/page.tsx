17fde1a784b25a746acf6b92dd9c476d6a846749
Your Name <you@example.com>
2025-02-19 17:51:58 +0100

    Remove styled-components and Header component, update font to Montserrat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import StarryBackground from "@/components/ui/StarryBackground/StarryBackground";
-
 export default function Home() {
   return (
     <>
-      <StarryBackground />
-      <div>
-        fdgsd
-      </div>
+     
     </>
   );
 }
