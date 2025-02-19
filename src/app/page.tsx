36486e97238cc9149476b75cc434d7d36c07e7fa
Your Name <you@example.com>
2025-02-19 17:17:39 +0100

    Add starry background component and update color scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import StarryBackground from "@/components/ui/StarryBackground/StarryBackground";
+
 export default function Home() {
   return (
-    <div >
-     
-    </div>
+    <main className="min-h-screen w-full relative">
+      <StarryBackground />
+      <div className="relative z-10">
+        {/* Contenu principal ici */}
+      </div>
+    </main>
   );
 }
---
+++ src/components/ui/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: '#F44336',
+    secondary: '#121c2e',
+    tertiary: '#1e293b',
+}
