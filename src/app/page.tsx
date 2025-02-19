0c9961a0738a44c7fc4ee5191f3ccd4c858d1e99
Your Name <you@example.com>
2025-02-19 17:33:30 +0100

    Refactor home page layout and remove main wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import StarryBackground from "@/components/ui/StarryBackground/StarryBackground"
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full relative">
+    <>
       <StarryBackground />
-      <div className="relative z-10">
-        {/* Contenu principal ici */}
+      <div>
+        fdgsd
       </div>
-    </main>
+    </>
   );
 }
